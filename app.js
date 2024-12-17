0fc0c9b3076f141380d965f5bff0bc8d75b8cc59
MattiaNatella <mattianatella@gmail.com>
2024-12-17 17:28:10 +0100

    remove backspaces

--- app.js
+++ app.js
@@ -1,34 +1,23 @@
 const {log} = require('console')
-
 const express = require('express')
 const app = express()
 const port = 3000
-
 const posts = require('./posts')
 
 app.use(express.static('public'));
-
 
 app.get('/', (req,res) => {
   res.send('Server del mio blog')
 })
 
 app.get('/bacheca', (req,res) => {
-
   const bacheca ={
     posts: posts,
     numeroPost: posts.length
   }
-  
   res.json(bacheca)
   log(bacheca)
- 
   })
-
-  // const bacheca = [{...posts}, {posts.length}]
-  // res.json(bacheca)
-  // log(bacheca)
-  
 
 app.get('/img', (req,res) => {
   res.send('<img src="public/ciambellone.jpeg">') //test pubblicazione assets statici
